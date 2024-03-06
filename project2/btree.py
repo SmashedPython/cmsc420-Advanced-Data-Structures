9bd37e2f7591eed9687f77fd69ada48819598520
SmashedPython <hongyangdu182@gmail.com>
2024-03-06 18:48:01 -0500

    complete insertion

--- project2/btree.py
+++ project2/btree.py
@@ -38,11 +38,10 @@ class Btree():
         return json.dumps(dict_repr,indent=2)
     
 
-    def insert_withspace(self,node,key,value):
-        m = self.m
+    def insert_helper(self,node,key,value):
         succes_insert = False
 
-        for i in range(len(node.keys) - 1):
+        for i in range(len(node.keys)):
             if node.keys[i] > key:
                 node.keys.insert(i,key)
                 node.values.insert(i,value)
@@ -51,126 +50,140 @@ class Btree():
         
         if not succes_insert:
             node.keys.append(key)
-            node.values.append(value)
-        
+            node.values.append(value)  
 
-    def find_parent_index(node):
+    def find_parent_index(self,node):
         p = node.parent
         target = node.keys[0]
-        for i in range(parent.keys):
-            if parent.keys > target:
+        for i in range(len(p.keys)):
+            if p.keys[i] > target:
                 return i
         
-        return i
+        return len(p.keys)
+
+    def left_rotate(self, node, index):
+        node.children[index].keys.append(node.keys[index ])
+        node.children[index].values.append(node.values[index ])
+
+        # print("here is the node being promote", node.children[index+1].keys)
+        node.keys[index] = node.children[index+1].keys.pop(0)
+
+        node.values[index] = node.children[index+1].values.pop(0)
+
+    def right_rotate(self, node, index):
+        node.children[index + 1].keys.insert(0, node.keys[index])
+        node.children[index + 1].values.insert(0,node.values[index])
+
+        node.keys[index] = node.children[index].keys.pop(-1)
+        node.values[index] = node.children[index].values.pop(-1)
+
+    def split_promote(self, node):
+        m = self.m
+
+        if len(node.keys) <= m-1:
+            return
+        else:
+            if node != self.root:
+                parent = node.parent
+
+                median_key = node.keys[(m-1)//2]
+                median_value = node.values[(m-1)//2]
+
+                newnode1 = Node(keys=node.keys[0:(m-1)//2], values= node.values[0:(m-1)//2], children= node.children[0:(m-1)//2 + 1], parent = parent)
+                newnode2 = Node(keys=node.keys[(m-1)//2 + 1:], values= node.values[(m-1)//2 + 1:], children= node.children[(m-1)//2 + 1:], parent = parent)
+
+                self.insert_helper(parent,median_key, median_value)
+                for i in range(len(parent.children)):
+                    if median_key in parent.children[i].keys:
+                        parent.children.pop(i)
+                        parent.children.insert(i,newnode2)
+                        parent.children.insert(i,newnode1)
+                        break
+                
+                self.split_promote(parent)
+            else:
+                median_key = node.keys[(m-1)//2]
+                median_value = node.values[(m-1)//2]
+
+                newroot = Node(keys=[median_key], values=[median_value],children=[])
+                newnode1 = Node(keys=node.keys[0:(m-1)//2], values= node.values[0:(m-1)//2], children= node.children[0:(m-1)//2 + 1], parent=newroot)
+                newnode2 = Node(keys=node.keys[(m-1)//2 + 1:], values= node.values[(m-1)//2 + 1:], children= node.children[(m-1)//2 + 1:], parent=newroot)
+                
+                # update parents since we created new object
+                for child in newnode1.children:
+                    child.parent = newnode1
+                for child in newnode2.children:
+                    child.parent = newnode2
+
+                newroot.children.append(newnode1)
+                newroot.children.append(newnode2)
+
+                self.root = newroot
+                return
+
+    def is_leaf(self, node):
+        if node.children == None or len(node.children) == 0:
+            return True
+        return False
+
 
     # Insert.
     def insert(self, key: int, value: str):
         if self.root == None:
-            self.root = Node(keys=[key], values=[value])
+            self.root = Node(keys=[key], values=[value],children=[])
             return
 
         m = self.m
 
         # Go to the leaf
         current = self.root
-        while not is_leaf(current):
-            for i in range(len(current.keys)):
+
+        while not self.is_leaf(current):
+
+            i = 0
+            while i < len(current.keys):
 
                 if current.keys[i] > key:
                     current = current.children[i]
                     break
-            if i == range(len(current.keys)):
-                current = current.children[i+1]
+                i += 1
+            if i == len(current.keys):
+                current = current.children[i]
 
         #now current is a leaf
         if len(current.keys) < m - 1:
             # if it has extra space
-            insert_withspace(current, key, value)
+            self.insert_helper(current, key, value)
         else:
+
+            if self.root == current: # case we only have a root and it is full
+                self.insert_helper(current,key,value)
+                self.split_promote(current)
+                return
+
             # try rotation first
-            i = find_parent_index(current)
+            i = self.find_parent_index(current)
             parent = current.parent
-            if i-1 > 0 and parent.children[i - 1] < m - 1:
+            # print(parent.keys)
+            # print(self.dump())
+            # print(parent.children)
+            # print("here0")
+            if i-1 >= 0 and len(parent.children[i - 1].keys) < m - 1:
                 # left has space
-                left_rotate(parent, i-1)
-            elif i < len(parent.children) parent.children[i] < m - 1:
-                right_rotate(parent, i-1)
+
+                self.insert_helper(current,key,value)
+                self.left_rotate(parent, i-1)
+
+            elif i < len(parent.children) - 1 and len(parent.children[i + 1].keys)< m - 1:
+                # right has space
+
+                self.insert_helper(current,key,value)
+                self.right_rotate(parent, i)
             else:
                # if there is no space in adjacent siblings
 
-
-            
-
-
-        # try rotation first 
-        
-
-        # if root is full, create a new root
-
-        # if len(root.keys) == m - 1:
-        #     newroot = Node(children= [])
-        #     self.root = newroot
-        #     newroot.children.insert(0,root)
-        #     self.split_child(newroot,0)
-        #     self.insert_unfull(node=newroot,key=key,value=value)
-        # else:
-        #     self.insert_unfull(root,key,value)
-
-    # def split_child(self, node, index):
-    #     m = self.m
-
-    #     child = node.children[index]
-    #     newnode = Node()
-    #     node.children.insert(index + 1,newnode)
-
-    #     #promote the middle one into the node
-    #     node.keys.insert(index, child.keys[m//2])
-    #     node.values.insert(index, child.values[m//2])
-
-    #     #split into 2
-    #     child.keys = child.keys[0 : m//2 - 1]
-    #     newnode.keys = child.keys[m//2 + 1: ]
-    #     child.values = child.values[0 : m//2 - 1]
-    #     newnode.values = child.values[m//2 + 1: ]
-
-    #     if not check_leaf(child):
-    #         newnode.children = child.children[m//2: ]
-    #         child.children = child.children[ :m//2-1 ]
-
-
-
-    def check_leaf(node):
-        if node.children == None or  node.children == []:
-            return True
-        return False
-
-    def insert_unfull(self,node,key,value):
-        m = self.m
-        i = len(node.keys)-1
-
-        
-        if check_leaf(node):
-            # if the node is a leaf, we add key/value into the node
-            node.keys.append(None)
-            node.values.append(None)
-            while i >= 0 and key < node.keys[i]:
-                node.keys[i+1] = node.keys[i]
-                node.values[i+1] = node.values[i]
-
-                i -= 1
-            node.keys[i+1] = key
-            node.values[i+1] = value
-        else:
-            # if it is not a leaf, we find the correct subtree
-            while i >= 0 and key < node.keys[i]:
-                i -= 1
-            i += 1
-            # if child node is full, split it
-            if len(node.children[i]) == m-1:
-                self.split_child(node,i)
-                if key > x.keys[i]:
-                    i += 1
-            self.insert_unfull(node = node.children[i],key=key,value=value)
+                self.insert_helper(current,key,value)
+                self.split_promote(current)
 
 
 
@@ -184,3 +197,25 @@ class Btree():
     def search(self,key) -> str:
         # Fill in and tweak the return.
         return json.dumps(None)
+
+
+t = Btree(3)
+t.insert(1,1)
+t.insert(2,2)
+t.insert(3,3)
+t.insert(0,0)
+t.insert(4,4)
+
+t.insert(5,5)
+
+t.insert(6,6)
+t.insert(-1,-1)
+
+t.insert(-2,-2)
+print("root",t.root.keys)
+print(t.dump())
+t.insert(7,7)
+
+print(t.dump())
+
+

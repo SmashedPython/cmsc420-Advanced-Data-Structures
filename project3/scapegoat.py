74f84fdbdd8d0cb3677644b9238359c7dd3c15ef
SmashedPython <hongyangdu182@gmail.com>
2024-03-16 16:55:07 -0400

    '

--- project3/scapegoat.py
+++ project3/scapegoat.py
@@ -52,14 +52,156 @@ class SGtree():
             dict_repr = _to_dict(self.root)
         return json.dumps(dict_repr,indent=2)
 
-    def insert(self, key: int, value: str):
-        # Fill in the details.
-        print(f'Insert: {key},{value}') # This is just here to make the code run, you can delete it.
+    def size(self,node):
+        if node is None:
+            return 0
 
+        leftsize = self.size(node.leftchild)
+        rightsize = self.size(node.rightchild)
+        return leftsize + rightsize + 1
+
+    def depth(self,node):
+        if node is None:
+            return -1
+
+        leftsize = self.depth(node.leftchild)
+        rightsize = self.depth(node.rightchild)
+        return max(leftsize,rightsize) + 1
+
+    def reconstruct(self, node):
+
+        def in_order_traverse(root):
+            if root == None:
+                return []
+
+            return in_order_traverse(root.leftchild) + [(root.key,root.value)] + in_order_traverse(root.rightchild)
+
+        
+        def construct(pairs):
+            if not pairs:
+                return None
+            mid = len(pairs)//2
+            root = Node(key = pairs[mid][0], value=  pairs[mid][1])
+
+            root.leftchild = construct(pairs[:mid])
+            if(root.leftchild != None):
+                root.leftchild.parent = root
+
+            root.rightchild = construct(pairs[mid+1:])
+            if(root.rightchild != None):
+                root.rightchild.parent = root
+            return root
+        
+        # Remove the next line and fill in code to restructure and assign the newroot.
+        pairs = in_order_traverse(node)
+        newroot = construct(pairs=pairs)
+        return(newroot)
+
+    def insert(self, key: int, value: str):
+        if self.root == None:
+            self.root = Node(key,value)
+            self.m = 1
+            self.n = 1
+            return
+        
+        #perform insertion
+        node = Node(key = key, value = value)
+
+        prev = None
+        current = self.root
+
+        while current != None:
+            prev = current
+
+            if current.key > key:
+                current = current.leftchild
+            else:
+                current = current.rightchild
+
+        node.parent = prev
+        if prev.key > key:
+            prev.leftchild = node
+        else:
+            prev.rightchild = node
+
+        self.n += 1
+        self.m += 1
+        ratio = self.a/self.b
+        d = self.depth(self.root)
+
+        # check for scapegoat
+        if d > math.log(self.n,1/ratio):
+            # go back to find the scapegoat
+            p = prev
+            while p != None:
+                if self.size(p.leftchild)/self.size(p) > ratio or self.size(p.rightchild)/self.size(p) > ratio:
+                    newroot = self.reconstruct(p)
+                    if p == self.root:
+                        self.root = newroot
+                    else:
+                        newroot.parent = p.parent
+
+                        if p.parent.key > newroot.key:
+                            p.parent.leftchild = newroot
+                        else:
+                            p.parent.rightchild = newroot
+                            
+                    break
+
+                else: p = p.parent
+
+    def delete_helper(self, root, key):
+        def find_inorder_successor(node):
+            current = node
+            while current.leftchild != None:
+                current = current.leftchild
+            return current
+
+        if root == None:
+            return None
+        
+        if key < root.key:
+            root.leftchild = self.delete_helper(root.leftchild,key)
+        elif key > root.key:
+            root.rightchild = self.delete_helper(root.rightchild,key)
+        else:
+            if root.leftchild == None:
+                if root.rightchild != None:
+                    root.rightchild.parent = root.parent
+                return root.rightchild
+            
+            elif root.rightchild == None:
+                if root.leftchild != None:
+                    root.leftchild.parent = root.parent
+                return root.leftchild
+
+                
+            temp = find_inorder_successor(root.rightchild)
+            root.key = temp.key
+            root.value = temp.value
+            root.rightchild = self.delete_helper(root.rightchild, temp.key)
+
+        return root
+    
     def delete(self, key: int):
-        # Fill in the details.
-        print(f'Delete: {key}') # This is just here to make the code run, you can delete it.
+        self.root = self.delete_helper(self.root,key)
+        self.n -= 1
+        if self.n < float(self.a / self.b * self.m):
+            self.root = self.reconstruct(self.root)
+            self.m = self.n
+
+
 
     def search(self, search_key: int) -> str:
-        # Fill in and tweak the return.
-        return json.dumps(None)
+        value_list = []
+        current = self.root
+        while current.key != search_key:
+            value_list.append(current.value)
+            if current.key > search_key:
+                current = current.leftchild
+            else:
+                current = current.rightchild
+
+        value_list.append(current.value)
+        return  json.dumps(value_list)
+
